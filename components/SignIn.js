8fea972d03992fff5acfdaf468d84a5e2c84a3e7
Nicolas Vallee <hello@nicolasvallee.dev>
2021-04-09 15:27:53 +0400

    update type button for Google signin button

--- components/SignIn.js
+++ components/SignIn.js
@@ -51,7 +51,7 @@ const SignIn = () => {
         />
         <S.AuthFormButtonsContainer>
           <CustomButton type='submit'>Sign In</CustomButton>
-          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+          <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>
             Sign in with Google
           </CustomButton>
         </S.AuthFormButtonsContainer>
