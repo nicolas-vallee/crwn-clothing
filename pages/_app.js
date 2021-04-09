7bc5a66af66dddb36543ccda5ab8d2f38b17b7d5
Nicolas Vallee <hello@nicolasvallee.dev>
2021-04-09 14:37:33 +0400

    update App to store user data

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,27 @@ import React, { useState, useEffect } from 'react';
 import GlobalStyle from '../styles/globalStyles';
 import Header from '../components/Header';
 
-import { auth } from '../firebase/firebase.utils';
+import { auth, createUserProfileDocument } from '../firebase/firebase.utils';
 
 function MyApp({ Component, pageProps }) {
   const [currentUser, setCurrentUser] = useState(null);
 
   useEffect(() => {
-    const unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      setCurrentUser(user);
-      console.log(user);
+    const unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          setCurrentUser({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      } else {
+        setCurrentUser(null);
+      }
     });
 
     return () => {
